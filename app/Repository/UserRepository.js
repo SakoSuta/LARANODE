10e596863f7e8c838adf363f250a1775eda66a54
SakoSuta <SakoSuta.em@gmail.com>
2023-12-16 18:02:28 +0100

    Add UserRepository class with CRUD operations

---
+++ app/Repository/UserRepository.js
@@ -0,0 +1,55 @@
+const pool = require('../path/to/your/database/connection'); //TODO: Remplacer par la connexion à votre base de données
+
+class UserRepository {
+    async createUser(userData) {
+        const query = 'INSERT INTO users(name, email, password, created_at, updated_at) VALUES($1, $2, $3, $4, $5) RETURNING *';
+        const values = [userData.name, userData.email, userData.password, new Date(), new Date()];
+        const result = await pool.query(query, values);
+        return result.rows[0];
+    }
+
+    async getUserById(userId) {
+        const query = 'SELECT * FROM users WHERE id = $1';
+        const values = [userId];
+        const result = await pool.query(query, values);
+        return result.rows[0];
+    }
+
+    async updateUser(userId, updateData) {
+        const set = [];
+        const values = [];
+        let paramIndex = 1;
+    
+        if (updateData.name) {
+            set.push(`name = $${paramIndex}`);
+            values.push(updateData.name);
+            paramIndex++;
+        }
+        if (updateData.email) {
+            set.push(`email = $${paramIndex}`);
+            values.push(updateData.email);
+            paramIndex++;
+        }
+    
+        set.push(`updated_at = $${paramIndex}`);
+        values.push(new Date());
+    
+        if (set.length === 0) {
+            throw new Error("No data provided to update");
+        }
+    
+        const query = `UPDATE users SET ${set.join(', ')} WHERE id = $${paramIndex}`;
+        values.push(userId);
+    
+        await pool.query(query, values);
+    }
+    
+
+    async deleteUser(userId) {
+        const query = 'DELETE FROM users WHERE id = $1';
+        const values = [userId];
+        await pool.query(query, values);
+    }       
+}
+
+module.exports = UserRepository;
