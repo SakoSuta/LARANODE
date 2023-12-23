9afed6fdcd5a22c38a0f2c9595b9ba30db79136b
SakoSuta <SakoSuta.em@gmail.com>
2023-12-23 14:05:05 +0100

    Update environment and database configuration

--- bootstrap/Db.js
+++ bootstrap/Db.js
@@ -1,11 +1,18 @@
 const { Pool } = require('pg');
+require('dotenv').config();
+
+const dbUsername = process.env.DB_USERNAME;
+const dbHost = process.env.DB_HOST;
+const dbName = process.env.DB_NAME;
+const dbPort = process.env.DB_PORT
+const dbPassword = process.env.DB_PASSWORD
 
 const pool = new Pool({
-    user: 'your_username',
-    host: 'localhost',
-    database: 'your_database_name',
-    password: 'your_password',
-    port: 'Port_number',
+    user: dbUsername,
+    host: dbHost,
+    database: dbName,
+    password: dbPassword,
+    port: dbPort,
 });
 
 module.exports = pool;
