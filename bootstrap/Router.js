43aba88b2d5db6bbb0d8995bf8d0fbf4bb26e8ac
SakoSuta <SakoSuta.em@gmail.com>
2023-12-23 13:40:40 +0100

    Add Router class to handle routing

---
+++ bootstrap/Router.js
@@ -0,0 +1,18 @@
+import Routes from "../routes/index";
+
+class Router {
+  constructor(server) {
+    this.server = server;
+    this.initializeRouting();
+  }
+
+  initializeRouting() {
+    this.server.on("request", this.routeRequest.bind(this));
+  }
+
+  routeRequest(req, res) {
+    Routes(req, res);
+  }
+}
+
+export default Router;
